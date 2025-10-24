e235d111f1344a845605e01f80d4fb5db1340740
cocorico8 <cocomarti0208@gmail.com>
2025-10-24 21:40:17 +0200

    oops

--- Hypervisor-Phantom/modules/auto_xml.py
+++ Hypervisor-Phantom/modules/auto_xml.py
@@ -77,7 +77,8 @@ class XMLGenerator:
             cpu_vendor: The CPU vendor string (e.g., 'GenuineIntel').
         """
         self.cpu_vendor = cpu_vendor
-        self.firmware_dir = core.OUTPUT_DIR / "firmware"
+        self.project_root = Path.cwd().resolve()
+        self.firmware_dir = self.project_root / core.OUTPUT_DIR / "firmware"
         self.template_dir = paths.XML_TEMPLATE_DIR
 
         # Paths to required firmware files
